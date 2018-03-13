bd5db8d999d6f88b671923c4cc2c56dbd0304fde
Tabish <tabishbashir@gmail.com>
2018-03-13 14:23:12 +0530

    Adding key transition effects

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 window.addEventListener('keydown',function(e){
       var audio = document.querySelector(`audio[data-key ="${e.keyCode}" ]`)
+      var key = document.querySelector(`.key[data-key ="${e.keyCode}" ]`)
+
+      if(audio == null) return;
+      key.classList.add('playing');
+      audio.currentTime = 0;
       audio.play();
+
+      var keys = document.querySelectorAll('.key');
+      keys.forEach(function(k){
+              console.log(k);
+              k.addEventListener('transitionend',removeTransition)});
+
+      function removeTransition(){
+        this.classList.remove('playing');
+      }
 })
